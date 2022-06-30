07ffa6723a253591176aeaab000a4d0fed4d7371
Edilhan <edylhan@gmail.com>
2022-06-30 10:17:06 +0600

    new_rep

---
+++ files/json_.py
@@ -0,0 +1,16 @@
+import json
+
+dict_ = {
+        'products': [
+            {
+                'id': 1,
+                'name': 'snickers',
+                'price': 45,
+            },
+            {
+                'id': 2,
+                'name': 'bounty',
+                'price': 45,
+            }
+        ]
+        }
